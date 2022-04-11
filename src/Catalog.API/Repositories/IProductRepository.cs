37149e92e033ed7f3115aa28e25ebbf0a46a43f0
fhasheminasab <hasheminasab97@gmail.com>
2022-04-11 11:15:04 +0430

    db, repo, and the basics are coded.

---
+++ src/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        //query
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string productId);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        //command
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+
+
+    }
+}
